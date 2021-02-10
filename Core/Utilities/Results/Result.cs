3794bd134d9bd64e17ce302a4a9ec8d1aac2836b
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-10 21:52:06 +0300

    ✨ Add Result constructor

--- Core/Utilities/Results/Result.cs
+++ Core/Utilities/Results/Result.cs
@@ -6,6 +6,17 @@ namespace Core.Utilities.Results
 {
     public class Result : IResult
     {
+        
+        public Result(bool success, string message)
+        {
+            Message = message;
+            Success = success;
+        }
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
         public bool Success { get; }
 
         public string Message { get; }
