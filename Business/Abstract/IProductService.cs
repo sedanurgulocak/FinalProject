0aa99cd343c0782cad5a9c9f791c1816b625267f
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-10 23:21:59 +0300

    ♻️ Rearrange according to Product manager DataResult and inherit Result to DataResult

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -9,11 +9,11 @@ namespace Business.Abstract
 {
     public interface IProductService
     {
-        List<Product> GetAll();
-        List<Product> GetAllByCategoryId(int id);
-        List<Product> GetByUnitPrice(decimal min, decimal max);
-        List<ProductDetailDto> GetProductDetails();
-        Product GetById(int ProductId);
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int id);
+        IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max);
+        IDataResult<List<Product>> GetProductDetails();
+        IDataResult<Product> GetById(int ProductId);
         IResult Add(Product product);
 
     }
