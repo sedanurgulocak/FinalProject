d095c8c1b4d43e32ec42509e727bbe84c5ef8cfc
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-17 22:43:51 +0300

    ✨Add FluentValidation operations

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,9 +1,11 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -20,11 +22,6 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
-            //Business codes
-            if (product.ProductName.Length<2)
-            {
-                return new ErrorResult(Messages.ProductNameInValid);
-            }
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
