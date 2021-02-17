4873525c9ab0f29ff829c4f01c13d3b7460624ed
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-17 21:32:43 +0300

    🔨Add ContainerBuilder

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Business.DependencyResolvers.Autofac
 {
-    class AutofacBusinessModule
+    public class AutofacBusinessModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+        }
     }
 }
