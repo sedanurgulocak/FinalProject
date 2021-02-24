aa952b06ecf29c4803005934aa93db6cb0ee6c85
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-25 00:20:08 +0300

    ♻️ Refactor code

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInValid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string ProductListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError="Bir kategoride en fazla 10 ürün bulunabilir";
+        public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
+        public static string CategoryLimitExceded="Kategori limiti aşıldı";
     }
 }
