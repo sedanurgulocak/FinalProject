76837ee4e9b535cdcacca4cc71b31efa4c5451eb
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-28 21:48:38 +0300

    ✨Add using constants

--- Business/BusinessAspects/Autofac/SecuredOperation.cs
+++ Business/BusinessAspects/Autofac/SecuredOperation.cs
@@ -7,6 +7,7 @@ using System.Text;
 using Castle.DynamicProxy;
 using Microsoft.Extensions.DependencyInjection;
 using Core.Extensions;
+using Business.Constants;
 
 namespace Business.BusinessAspects.Autofac
 {
