ab5886111cf5c7dee03e0fa14b12bd3cf737b3ab
sedanurgulocak <sedanurgulocak@gmail.com>
2021-02-28 22:24:23 +0300

    ✨Add new messages

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -15,5 +15,11 @@ namespace Business.Constants
         public static string ProductNameAlreadyExists="Bu isimde zaten başka bir ürün var";
         public static string CategoryLimitExceded="Kategori limiti aşıldı";
         public static string AuthorizationDenied = "Yetkiniz yok";
+        public static string UserRegistered = "Kayıt oldu";
+        public static string UserNotFound = "Kullanıcı bulunamadı";
+        public static string PasswordError = "Parola hatası";
+        public static string SuccessfulLogin = "Başarılı giriş";
+        public static string UserAlreadyExists = "Kullanıcı mevcut";
+
     }
 }
