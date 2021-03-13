38c808d6ab0acbc991153e029979464766fc9ef8
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-13 23:12:13 +0300

    🔨Add GetByCategory methof to ProductsController

--- WebAPI/Controllers/ProductsController.cs
+++ WebAPI/Controllers/ProductsController.cs
@@ -45,6 +45,18 @@ namespace WebAPI.Controllers
             return BadRequest(result);
         }
 
+        [HttpGet("getbycategory")]
+        public IActionResult GetByCategory(int id)
+        {
+            var result = _productService.GetAllByCategoryId(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+
         [HttpPost("add")]
         public IActionResult Add(Product product)
         {
